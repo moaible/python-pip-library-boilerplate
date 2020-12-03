6fbb92e4d84e42ce8fa2acd88f3cc78626189e38
Eimer <moai.motodera@gmail.com>
2020-12-03 08:56:50 +0000

    Add environment python files

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name="library-boilerplate",
+	version="0.1",
+	description="Test for pip install git+",
+	url="https://github.com/moaible/python-pip-library-boilerplate",
+	packages=find_packages(),
+	)
